46ad963d581407ef53a1db88dc7cc8e42a1565d0
Roman Hnatiuk <romgnatyuk@gmail.com>
2025-11-03 23:16:27 +0100

    Refactor laravel-echo-fns.ts to consolidate channel event handling into a single echo function, improving code maintainability and readability.

--- src/lib/laravel-echo-fns.ts
+++ src/lib/laravel-echo-fns.ts
@@ -1,20 +1,33 @@
 import {inject} from "@angular/core";
 import {LaravelEcho} from "./laravel-echo";
+import {Broadcaster, BroadcastDriver} from "laravel-echo";
+
+type Visibility = 'public' | 'private' | 'presence';
+
+const echo = <TPayload>(channelName: string, eventName: string, callback: (data: TPayload) => void, visibility: Visibility = 'public') => {
+  const echo = inject(LaravelEcho).getEcho();
+  const channels = {
+    public: echo.channel(channelName),
+    private: echo.private(channelName),
+    presence: echo.join(channelName),
+  };
+
+  if (visibility === 'presence') {
+    channels[visibility].whisper(eventName, callback);
+  } else {
+    channels[visibility].listen(eventName, callback);
+  }
+  return { stopListening: () => channels[visibility].stopListening(eventName) };
+}
 
 const laravelEcho = <TPayload>(channelName: string, eventName: string, callback: (data: TPayload) => void) => {
-  return inject(LaravelEcho).getEcho()
-    .channel(channelName)
-    .listen(eventName, callback);
+  return echo(channelName, eventName, callback, 'public');
 }
 
 const laravelEchoPrivate = <TPayload>(channelName: string, eventName: string, callback: (data: TPayload) => void) => {
-  return inject(LaravelEcho).getEcho()
-    .private(channelName)
-    .listen(eventName, callback);
+  return echo(channelName, eventName, callback, 'private');
 }
 
 const laravelEchoPresence = <TPayload>(channelName: string, eventName: string, callback: (data: TPayload) => void) => {
-  return inject(LaravelEcho).getEcho()
-    .join(channelName)
-    .whisper(eventName, callback);
+  return echo(channelName, eventName, callback, 'presence');
 }
